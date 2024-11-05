e80ea4c5d43c122b28e8746693f09478bf59e7e3
Shahzad Ali <shehzadali3th@gmail.com>
2024-11-05 12:41:46 -0800

    Add new files

---
+++ fcs_forex.py
@@ -0,0 +1,217 @@
+import requests
+
+class FCSForex:
+    def __init__(self, api_key=''):
+        self.api_key = api_key if api_key else "API_KEY"
+        self.output = ''  # default is json
+        self.output_type = 'JSON'  # Default output type JSON
+        self.basic_url = "https://fcsapi.com/api-v3/forex"
+        self.api_message = "API Key is empty, please set your API Key."
+
+
+    def return_error(self, msg):
+        return {"status":False,"msg":msg,"Error":"Code or input data error"}
+    
+
+    def check_api_key(self):
+        """Check if the API key is valid."""
+        return self.api_key is not None and self.api_key != 'api_key'
+
+    def check_symbol_id(self, txt):
+        """Determine if the input is an ID or symbol."""
+        cleaned_txt = txt.replace(",", "")  # Remove commas
+        return 'id' if cleaned_txt.isdigit() else 'symbol'
+
+    def response(self, url, params):
+        """Make a request and return the JSON response."""
+        if not self.check_api_key():
+            return self.return_error(self.api_message)
+        
+        if self.api_key:
+            params['access_key'] = self.api_key
+        if self.output:
+            params['output'] = self.output
+
+        response = requests.post(url, data=params)
+
+        # Check if response is JSON
+        try:
+            response_data = response.json()
+            return response_data  # Return JSON data without prettifying
+        except ValueError:
+            # If response is not JSON, return it as is
+            return self.return_error(response.text)
+
+    def get_symbols_list(self,type='forex'):
+        """Return all symbols list of forex of ids and symbol."""
+        if type != 'forex' and type != 'crypto':
+            type = 'forex'
+        params = {'type': type}
+        link = f"{self.basic_url}/list"
+        return self.response(link, params)
+
+    def get_profile(self, symbol):
+        """Get the details or profile of Currency."""
+        symbol = ','.join(symbol) if isinstance(symbol, list) else symbol # convert [1,2,3] to 1,2,3
+
+        if not symbol:
+            return self.return_error("Symbol or Id not defined")
+
+        symbol_id = self.check_symbol_id(symbol) #
+        params = {symbol_id: symbol}
+
+        link = f"{self.basic_url}/profile"
+        return self.response(link, params)
+
+    def get_converter(self, amount='200', pair_one='', pair_two=''):
+        """Convert Base to quote currency."""
+        if not pair_one:
+            return self.return_error("Symbol not defined")
+
+        params = {'amount': amount}
+
+        if pair_two:
+            params['pair1'] = pair_one
+            params['pair2'] = pair_two
+        else:
+            params["symbol"] = pair_one
+
+        link = f"{self.basic_url}/converter"
+        return self.response(link, params)
+
+    def get_latest_price(self, symbol):
+        """Get Forex Latest Price By id or symbol."""
+        symbol = ','.join(symbol) if isinstance(symbol, list) else symbol
+
+        if not symbol:
+            return self.return_error("Symbol or Id not defined")
+
+        symbol_id = self.check_symbol_id(symbol)
+        params = {symbol_id: symbol}
+
+        link = f"{self.basic_url}/latest"
+        return self.response(link, params)
+
+    def get_base_prices(self, symbol, type="forex", time=False):
+        """Get all quote prices."""
+        symbol = ','.join(symbol) if isinstance(symbol, list) else symbol
+
+        if not symbol:
+            return self.return_error("Symbol or Id not defined")
+
+        params = {'symbol': symbol, 'type': type}
+        if time:
+            params['time'] = 1
+
+        link = f"{self.basic_url}/base_latest"
+        return self.response(link, params)
+
+    def get_last_candle(self, symbol, period='1h'):
+        """Get Candle price by time period."""
+        symbol = ','.join(symbol) if isinstance(symbol, list) else symbol
+
+        if not symbol:
+            return self.return_error("Symbol or Id not defined")
+
+        symbol_id = self.check_symbol_id(symbol)
+        params = {symbol_id: symbol, 'period': period}
+
+        link = f"{self.basic_url}/candle"
+        return self.response(link, params)
+
+    def get_history(self, data):
+        """Get specific currency history data."""
+        id      = data.get('id', '')
+        symbol  = data.get('symbol', '')
+
+        period  = data.get('period', '1h')
+        level   = data.get('level', 1)
+        if (level < 1 or level > 3) : level = 1
+   
+        from_date = data.get('from', '')
+        to_date = data.get('to', '')
+
+        if not symbol and not id:
+            return self.return_error("Symbol or Id not defined")
+
+        params = {
+            'period': period,
+            'limit': level
+        }
+        if id: 
+            params['id'] = id
+        if symbol:
+            params['symbol'] = symbol
+
+        if from_date and to_date:
+            params['from'] = from_date
+            params['to'] = to_date
+
+        link = f"{self.basic_url}/history"
+        return self.response(link, params)
+
+    def get_signal(self,symbol,period, url):
+        if not symbol:
+            return self.return_error("Symbol or Id not defined")
+
+        result = False if isinstance(symbol, list) else False if ',' in symbol else True
+        if result is not True: 
+            return self.return_error("Symbol only accept single id")
+
+        symbol_id = self.check_symbol_id(symbol)
+        params = {symbol_id: symbol, 'period': period}
+
+        link = f"{self.basic_url}/"+url
+        return self.response(link, params)
+
+
+    def get_pivot_points(self, symbol, period='1h'):
+        """Get pivot points."""
+        return self.get_signal(symbol,period,"pivot_points")
+
+    def get_moving_averages(self, symbol, period='1h'):
+        """Get moving averages forex."""
+        return self.get_signal(symbol,period,"ma_avg")
+
+    def get_technical_indicator(self, symbol, period='1h'):
+        """Get top indicators signals."""
+        return self.get_signal(symbol,period,"indicators")
+
+    def get_economy_calendar(self, symbol='', country='', from_date='', to_date='', event=''):
+        """Get economy calendar by symbol, country, date range, or event."""
+        if not self.check_api_key():
+            return self.return_error(self.api_message)
+
+        # Prepare parameters based on the input
+        params = {'access_key': self.api_key}
+        if from_date:
+            params['from'] = from_date
+        if to_date:
+            params['to'] = to_date
+        if symbol:
+            params['symbol'] = symbol
+        if country:
+            params['country'] = country
+        if event:
+            params['event'] = event  # Add event parameter
+
+        # Validate that at least one of symbol, country, or event is provided
+        if not symbol and not country and not event:
+            return self.return_error("At least a symbol, country, or event must be defined")
+
+        link = f"{self.basic_url}/economy_cal"
+        return self.response(link, params)
+
+
+    def get_search_query(self, search, strict=0):
+        """Search API."""
+
+        if not search:
+            return self.return_error("Search value in empty")
+
+        params = {'s': search, 'strict': strict}
+        link = f"{self.basic_url}/search"
+        return self.response(link, params)
+
+
+    
---
+++ main.py
@@ -0,0 +1,134 @@
+# fcs_forex/main.py
+# Import FCS API Class
+from fcs_forex import FCSForex
+
+def main():
+
+    # An API key, you can get free at https://fcsapi.com/dashboard
+    forex_api = FCSForex(api_key='XxNTqX6UlRgDaIo3N24M')  
+
+    # Example: Get symbols list, Documentation : https://fcsapi.com/document/forex-api#forexsupportedcurrency
+    # symbols_list = forex_api.get_symbols_list()
+    # print("Symbols List:", symbols_list)
+
+    # # Example: Get profile of a currency
+    # symbol_profile = forex_api.get_profile('1')
+    # print("Currency Profile of your desired country :", symbol_profile)
+
+    # symbol_profile = forex_api.get_profile('1,2,3')
+    # print("Currency Profile BY IDS:", symbol_profile)
+
+
+    # # Example: Convert currency
+    # conversion_result = forex_api.get_converter('200','JPY/GBP')
+    # print("Conversion Result :", conversion_result)
+
+    conversion_result = forex_api.get_converter(200, 'EUR','USD')
+    print("Conversion Result for different countries:", conversion_result)
+
+    # # Get Latest Price
+    # latest_price = forex_api.get_latest_price("all_forex")
+    # print("Latest Price for all_forex :", latest_price)
+
+    # latest_price = forex_api.get_latest_price("JPY/GBP")
+    # print("Latest Price for different countries:", latest_price)
+
+    # latest_price = forex_api.get_latest_price(['EUR/USD','JPY/USD'])
+    # print("Latest Price for multiple countries:", latest_price)
+
+    # latest_price = forex_api.get_latest_price("87") 
+    # print("Latest Price by ID:", latest_price) 
+
+
+    # # Get Base Prices
+    # base_prices = forex_api.get_base_prices("USD")
+    # print("Base Prices for Different countries:", base_prices)
+
+    # base_prices = forex_api.get_base_prices('EUR','crypto')
+    # print("Base Prices for EUR and forex:", base_prices)
+
+    # # Get Last Candle
+    # last_candle = forex_api.get_last_candle('1,2,3','1d')
+    # print("Last Candle for EURUSD:", last_candle)
+
+    # last_candle = forex_api.get_last_candle("all_forex", '1h')
+    # print("Last Candle for EURUSD:", last_candle)
+
+    # last_candle = forex_api.get_last_candle("all_forex", '1d')
+    # print("Last Candle for EURUSD:", last_candle)
+
+    # last_candle = forex_api.get_last_candle("all_forex", '1d')
+    # print("Last Candle for EURUSD:", last_candle)
+
+    # candle_data = forex_api.get_last_candle("1,2,3", period='1h')
+    # print("Candle Data for IDs:", candle_data)
+
+
+    # # Get Currency History
+    # history_data = forex_api.get_history({
+    #     'id': '10',
+    #     'period': '1d',
+    #     'level': 1,
+    #     'from': '2024-10-30',
+    #     'to': '2024-10-31'
+    # })
+    # print("History Data for countries:", history_data)
+
+    # history_data = forex_api.get_history({
+    # 'id': '3',
+    # 'period': '1h'
+    # })
+    # print("History Data for countries:", history_data)
+
+
+    # # Get Pivot Points
+
+    # pivot_points = forex_api.get_pivot_points("10", '1d')
+    # print("Pivot Points for EURUSD:", pivot_points)
+
+    # pivot_points = forex_api.get_pivot_points("EUR/USD", '1d')
+    # print("Pivot Points for countries:", pivot_points)
+
+    # pivot_points = forex_api.get_pivot_points("JPY/CHF", '1d')
+    # print("Pivot Points for countries:", pivot_points)
+
+
+    # # Get Moving Averages
+    # moving_averages = forex_api.get_moving_averages("10", '1d')
+    # print("Moving Averages for 1:", moving_averages)
+
+    # moving_averages = forex_api.get_moving_averages("JPY/CHF", '1d')
+    # print("Moving Averages for different countries:", moving_averages)
+
+
+    # # Get Technical Indicators
+    # indicators = forex_api.get_technical_indicator('10', '1d')
+    # print("Technical Indicators for id and symbol:", indicators)
+
+    # indicators = forex_api.get_technical_indicator("JPY/CHF", period='1d')
+    # print("Technical Indicators for countries code:", indicators)
+
+    # Get Economy Calendar
+    # economy_calendar = forex_api.get_economy_calendar(country='US,JP', from_date='2024-11-04', to_date='2024-11-05')
+    # print("Economy Calendar for countries  :", economy_calendar)
+
+    # economy_calendar_event = forex_api.get_economy_calendar(event='365014')
+    # print("Economy Calendar (Event):", economy_calendar_event)
+
+    # economy_calendar_range = forex_api.get_economy_calendar(symbol='USD', from_date='2024-11-04', to_date='2024-11-05')
+    # print("Economy Calendar (Date Range with Symbol):", economy_calendar_range)
+
+    # economy_calendar = forex_api.get_economy_calendar(country='US')
+    # print("Economy Calendar for countries  :", economy_calendar)
+
+    # # Search API
+    # search_results = forex_api.get_search_query("BTC")
+    # print("Search Results for 'EUR':", search_results)
+
+    # search_results = forex_api.get_search_query("BTC Dollar",1)
+    # print("Search Results for 'EUR':", search_results)
+
+    
+
+if __name__ == "__main__":
+    main()
